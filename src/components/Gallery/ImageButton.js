8007a9e3719a19324968bdfe146ceaa4432ebdb5
Numan <ahmet.aral@ucalgary.ca>
2021-02-19 19:53:59 -0700

    didn't end up using styled-components much

--- src/components/Gallery/ImageButton.js
+++ src/components/Gallery/ImageButton.js
@@ -1,30 +1,11 @@
 import React from "react";
-import styled from "styled-components";
-
-const BUTTON_SIZE = 50;
-
-const StyledButton = styled.button`
-  /* max-height: ${BUTTON_SIZE}px; */
-  /* max-width: ${BUTTON_SIZE}px; */
-  height: ${BUTTON_SIZE}px;
-  width: auto;
-  padding: 0;
-  overflow: hidden;
-  margin: 0 10px 10px 0;
-  border: none;
-  outline: 0;
-`;
-
-const StyledImage = styled.img`
-  /* width: 100%; */
-  height: 100%;
-`;
+import "./ImageButton.css";
 
 const ImageButton = ({ src, altText, onClick, ...rest }) => {
   return (
-    <StyledButton onClick={onClick} {...rest}>
-      <StyledImage src={src} alt={altText} />
-    </StyledButton>
+    <button className="image-button" onClick={onClick} {...rest}>
+      <img height="100%" src={src} alt={altText} />
+    </button>
   );
 };
 
