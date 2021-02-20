7182bf1e84aca0b7915596774bcf4a19a0c4d94a
Numan <ahmet.aral@ucalgary.ca>
2021-02-19 19:44:13 -0700

    update routes

--- src/index.js
+++ src/index.js
@@ -1,59 +1,31 @@
-/* eslint-disable import/no-unresolved */
-/*!
-
-=========================================================
-* Paper Kit React - v1.2.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/paper-kit-react
-
-* Copyright 2020 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/paper-kit-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 
-// styles
 import "assets/css/bootstrap.min.css";
-import "assets/scss/paper-kit.scss?v=1.2.0";
-import "assets/demo/demo.css?v=1.2.0";
-// pages
-import Index from "views/Index.js";
-import NucleoIcons from "views/NucleoIcons.js";
-import LandingPage from "views/examples/LandingPage.js";
-import ProfilePage from "views/examples/ProfilePage.js";
-import RegisterPage from "views/examples/RegisterPage.js";
-// others
+import "assets/scss/paper-kit.scss";
+import "assets/demo/demo.css";
+
+import Home from "views/Home";
+import Profile from "views/Profile";
+import Photojournalism from "views/Photojournalism";
+import Projects from "views/Projects";
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route path="/" exact render={(props) => <Index {...props} />} />
+      <Route path="/" exact render={(props) => <Home {...props} />} />
       <Route
-        path="/nucleo-icons"
-        render={(props) => <NucleoIcons {...props} />}
+        path="/photojournalism"
+        render={(props) => <Photojournalism {...props} />}
       />
-      <Route
-        path="/landing-page"
-        render={(props) => <LandingPage {...props} />}
-      />
-      <Route
-        path="/profile-page"
-        render={(props) => <ProfilePage {...props} />}
-      />
-      <Route
-        path="/register-page"
-        render={(props) => <RegisterPage {...props} />}
-      />
-      <Redirect to="/index" />
+      <Route path="/projects" render={(props) => <Projects {...props} />} />
+      {process.env.NODE_ENV !== "production" && (
+        <>
+          <Route path="/profile" render={(props) => <Profile {...props} />} />
+        </>
+      )}
+      <Redirect to="/" />
     </Switch>
   </BrowserRouter>,
   document.getElementById("root")
