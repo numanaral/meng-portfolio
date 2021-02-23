671ceba6c86d94ee43230ed098081ed69f02af29
Numan <ahmet.aral@ucalgary.ca>
2021-02-23 15:58:46 -0700

    add pdf icons, update config

--- src/containers/Projects/constants.js
+++ src/containers/Projects/constants.js
@@ -11,14 +11,42 @@ const tabs = [
   {
     title: "Reportage",
     content: Reportage,
+    props: {
+      documents: [
+        {
+          title: "Backscene magazine April 2020",
+          fileName: "backscene-magazine-april-2020",
+        },
+      ],
+    },
   },
   {
     title: "Communications",
     content: Communications,
+    props: {
+      documents: [
+        {
+          title: "Backscene magazine April to September 2020",
+          fileName: "backscene-magazine-april-to-september-2020",
+        },
+        {
+          title: "Backgrounder media kit",
+          fileName: "backgrounder-media-kit",
+        },
+      ],
+    },
   },
   {
     title: "Design",
     content: Design,
+    props: {
+      documents: [
+        {
+          title: "How did Uyghur conflict escalated in China",
+          fileName: "how-did-uyghur-conflict-escalated-in-china",
+        },
+      ],
+    },
   },
 ];
 
