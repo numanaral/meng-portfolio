1d89c7fc7a11c86dc3aa43f55e844d83e9af4472
Numan <ahmet.aral@ucalgary.ca>
2021-02-23 06:21:08 -0700

    add loading state + indicator to wakalet

--- src/containers/Projects/Wakalet.js
+++ src/containers/Projects/Wakalet.js
@@ -1,22 +1,37 @@
-import React from "react";
+import LoadingIndicator from "components/LoadingIndicator";
+import React, { useState } from "react";
 
-const Wakalet = () => (
-  <>
-    <iframe
-      title="wakalet"
-      className="wakeletEmbed"
-      width="100%"
-      height="500px"
-      src="https://embed.wakelet.com/wakes/lGu0kfEXADl4yNmLtXRFM/list"
-      style={{ border: "none" }}
-      allow="autoplay"
-    />
-    {/* <!-- Please only call https://embed-assets.wakelet.com/wakelet-embed once per page --> */}
-    <script
-      src="https://embed-assets.wakelet.com/wakelet-embed"
-      charSet="UTF-8"
-    />
-  </>
-);
+const Wakalet = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [height, setHeight] = useState("0%");
+  const onLoad = () => {
+    setIsLoading(false);
+    setHeight("100%");
+  };
+  return (
+    <>
+      {isLoading && (
+        <>
+          <b>Wakalet is loading...</b> <br /> <LoadingIndicator />
+        </>
+      )}
+      <iframe
+        title="Wakalet"
+        className="wakeletEmbed"
+        width="100%"
+        height={height}
+        src="https://embed.wakelet.com/wakes/lGu0kfEXADl4yNmLtXRFM/list"
+        style={{ border: "none", transition: "height 0.5s ease" }}
+        allow="autoplay"
+        onLoad={onLoad}
+      />
+      {/* <!-- Please only call https://embed-assets.wakelet.com/wakelet-embed once per page --> */}
+      <script
+        src="https://embed-assets.wakelet.com/wakelet-embed"
+        charSet="UTF-8"
+      />
+    </>
+  );
+};
 
 export default Wakalet;
