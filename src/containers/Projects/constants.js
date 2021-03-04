609fcc49624db6907dc3b8ed1ebcfd3debe6de07
Numan <ahmet.aral@ucalgary.ca>
2021-03-04 07:50:21 -0700

    add shorthands constants

--- src/containers/Projects/constants.js
+++ src/containers/Projects/constants.js
@@ -17,6 +17,38 @@ const tabs = [
           fileName: "how-did-uyghur-conflict-escalated-in-china",
         },
       ],
+      shorthands: [
+        {
+          fileName:
+            "calgary-parents--students--safety-depends-on-parents-and-children-themselves",
+          title:
+            "Calgary parents: Students' safety depends on parents and children themselves",
+        },
+        {
+          fileName:
+            "calgary-small-business-donates-winter-clothing---we-can-t-have-one-without-the-other-",
+          title:
+            "Calgary small business donates winter clothing: 'We can't have one without the other'",
+        },
+        {
+          fileName:
+            "sait-gets-ready-to-vote-on-campus-for-the-next-federal-election",
+          title:
+            "SAIT gets ready to Vote on Campus for the next federal election",
+        },
+        {
+          fileName:
+            "was-it-worth-the-hassle--calgarians-hope-to-see-improvements-in-privatized-driving-exam-services",
+          title:
+            "Was it worth the hassle? Calgarians hope to see improvements in privatized driving exam services",
+        },
+        {
+          fileName:
+            "-a-little-bit-of--normal-and--a-big-dose--of-safe--at--the-calgary-zoolights--during-the-pandemic",
+          title:
+            "A little bit of normal and a big dose of safe at the calgary Zoolights during the pandemic",
+        },
+      ],
     },
   },
   {
